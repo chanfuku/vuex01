8f13837f8ee873621d9684e9aa26437f403b062c
chanfuku <koheiebato@gmail.com>
2020-04-18 14:26:00 +0900

    修正

--- components/Navi.vue
+++ components/Navi.vue
@@ -1,10 +1,10 @@
 <template>
   <nav>
     <ul class="gnav">
-      <li>
+      <li :class="{current: getRoutePath() === '/'}">
         <a href="/"> <i class="fas fa-home"></i> <span>ホーム</span> </a>
       </li>
-      <li class="current">
+      <li :class="{current: getRoutePath() === '/allocate/allocate'}">
         <nuxt-link to="/allocate/allocate">
           <i class="far fa-calendar-alt"></i><span>スケジューラー</span>
         </nuxt-link>
@@ -12,6 +12,22 @@
     </ul>
   </nav>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {
+        }
+    }
+  },
+  methods: {
+      getRoutePath() {
+        return this.$route.path
+      }
+    }
+}
+</script>
 
 <style lang="scss">
 @import '~/assets/styles/nav.scss';
